d38df7f128221c581a148d3d2a7d231962e3bcde
Matias Zapata Rojas <matiaszptro@gmail.com>
2025-08-13 22:52:49 -0500

    Funciones de busqueda por filtros agregada

--- Practica.hs
+++ Practica.hs
@@ -10,20 +10,64 @@ data Libro = Libro {
     titulo :: String,
     autor :: String,
     categoria :: String,
+    estado :: String,
     entrada :: UTCTime,
     salida :: Maybe UTCTime
 }deriving (Show, Read)
 
+--Funcion Registrar Libro--
+registrarEntradaLibro :: Int -> String -> String -> String -> String -> UTCTime -> [Libro] -> [Libro]
+registrarEntradaLibro codigoLibro tituloLibro autorLibro categoriaLibro estadoLibro tiempo libreria =
+    Libro codigoLibro tituloLibro autorLibro categoriaLibro "Disponible" tiempo Nothing : libreria
 
-registrarEntradaLibro :: Int -> String -> String -> String -> UTCTime -> [Libro] -> [Libro]
-registrarEntradaLibro codigoLibro tituloLibro autorLibro categoriaLibro tiempo libreria =
-    Libro codigoLibro tituloLibro autorLibro categoriaLibro tiempo Nothing : libreria
-
-
-
+--Funcion Prestar Libro--
 registrarSalidaLibro :: Int -> UTCTime -> [Libro] -> [Libro]
 registrarSalidaLibro codigoLibro tiempo libreria =
-    map (\v -> if codigoLibro == codigo v then v{salida = Just tiempo} else v) libreria
+    map (\v -> if codigoLibro == codigo v
+               then v {salida = Just tiempo, estado = "Prestado"}
+               else v) libreria
+
+
+--Funcion Buscar Libro por Codigo--
+buscarLibroCodigo :: Int -> [Libro] -> String
+buscarLibroCodigo codigoLibro libreria =
+    let resultado = filter (\v -> codigoLibro == codigo v) libreria
+    in if null resultado
+       then "No hay ningun libro con este codigo"
+       else unlines (map mostrarLibro resultado)
+
+--Funcion Buscar Libro por Titulo--
+buscarLibroTitulo :: String -> [Libro] -> String
+buscarLibroTitulo tituloLibro libreria =
+    let resultado = filter (\v -> tituloLibro == titulo v) libreria
+    in if null resultado
+       then "No hay ningun libro con este titulo"
+       else unlines (map mostrarLibro resultado)
+
+--Funcion Buscar Libro por Autor--
+buscarLibroAutor :: String -> [Libro] -> String--Filer devuelve una lista--
+buscarLibroAutor autorLibro libreria =
+    let resultado = filter (\v -> autorLibro == autor v) libreria
+    in if null resultado
+       then "No hay libros de este autor en la biblioteca."
+       else unlines (map mostrarLibro resultado)
+
+--Funcion Buscar Libro por Categoria--
+buscarLibroCategoria :: String -> [Libro] -> String
+buscarLibroCategoria categoriaLibro libreria =
+    let resultado = filter (\v -> categoriaLibro == categoria v) libreria
+    in if null resultado
+       then "No hay libros de esta categoria"
+       else unlines (map mostrarLibro resultado)
+
+--Funcion Buscar Libro por Estado--
+buscarLibroEstado :: String -> [Libro] -> String
+buscarLibroEstado estadoLibro libreria =
+    let resultado = filter (\v -> estadoLibro == estado v) libreria
+    in if null resultado
+       then "No hay libros con este estado"
+       else unlines (map mostrarLibro resultado)
+
 
 
 
